c1cfc49d8500369264c066e1a69cd5fb01ec5be1
Matt <mathewsb.00@gmail.com>
2024-09-18 21:33:49 -0400

    Additional folders

---
+++ myTest/hello1.php
@@ -0,0 +1,14 @@
+<?php
+echo "<html>";
+echo "<head><title>HTML with PHP</title>";
+echo "</head>";
+echo "<body>My hello world example!";
+echo "</body>";
+
+//your php code here
+//Or you can use the print command
+
+//print "<i>Print works too!</i>;
+echo "</html>";
+?>
+
---
+++ myTest/hello2.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>Hello 2</title>
+    </head>
+    <body>
+        <h1>Hello 2 Example</h1>
+        <?php
+        echo "Hello World";
+        ?>
+        <br>
+        <b>Here is more HTML</b>
+        <hr>
+        <?php
+        print "Say Hi to everyone"
+        ?>
+    </body>
+</html>
